05a006115f27df50095f98857a650e41c01423c8
MoYuM <hanbaochang12138@gmail.com>
2022-01-16 23:09:49 +0800

    fix: 改用link

--- src/pages/home/components/Main/index.tsx
+++ src/pages/home/components/Main/index.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import mySelf from "@/articles/mySelf.md?raw";
 import Article from "../../../../components/Article";
+import { Link } from "react-router-dom";
 
 const Main: React.FC = () => {
   return (
     <div className="container mx-auto mt-10 mb-10">
       <Article>
         <ReactMarkdown>{mySelf}</ReactMarkdown>
-        <a href="./articles">
+        <Link to="./articles">
           <h1>文章</h1>
-        </a>
+        </Link>
       </Article>
     </div>
   );
